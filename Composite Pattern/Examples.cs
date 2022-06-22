023713e7046f5ffdd28e5613903aa40d50dbfc65
leonard.bolland <leonard.bolland@tc.de>
2022-06-22 09:11:56 +0100

    add file for code examples, some minor rewrites in other files

---
+++ Composite Pattern/Examples.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite_Pattern
+{
+    internal class Examples
+    {
+        // looking at ideas how to structure the code
+        /*Example1:*/void MainMenu()
+        {
+            int option1;
+            int option2;
+            //usw
+        }
+        // if option 1 is selected, go to SubMenu1()
+        void SubMenu1()
+        {
+            int option1;
+            int option2;
+            //usw
+            int backToMainMenu; //how?
+        }
+    }
+}
--- Composite Pattern/Program.cs
+++ Composite Pattern/Program.cs
@@ -115,32 +115,33 @@
                         while (manipulate == true)
                         {
                             choice = '0';
-                                // copypasted segment, create function
-                                while (new CharacterResponseValidation().CheckIfNumber(choice) == true && ((choice < '1') || (choice > '2')))
-                                {
-                                    Console.WriteLine("What do you wish to do?\n" +
-                                        "Press 1 to add a Box\n" +
-                                        "Press 2 to add a Product\n");
-                                    choice = Console.ReadKey().KeyChar;
-                                    Console.WriteLine("\n");
-                                }
-                                if (new CharacterResponseValidation().CheckIfNumber(choice) != true || (choice > '2'))
-                                {
-                                    Console.WriteLine("Invalid selection. Please try again.\n");
-                                }
-                                switch (choice)
-                                {
-                                    case '1':
-                                        box.add(new Box());
-                                        Console.WriteLine("Added a Box");
-                                        break;
-                                    case '2':
-                                        box.add(new Product());
-                                        Console.WriteLine("Added a Product");
-                                        break;
-                                }
-                                bool selectionValue = false;
-                                char selectionChar = '0';
+                            while (new CharacterResponseValidation().CheckIfNumber(choice) == true && ((choice < '1') || (choice > '2')))
+                            {
+                                Console.WriteLine("What do you wish to do?\n" +
+                                    "Press 1 to add a Box\n" +
+                                    "Press 2 to add a Product\n");
+                                choice = Console.ReadKey().KeyChar;
+                                Console.WriteLine("\n");
+                            }
+                            // if statement not working properly for some reason --> right now it takes too much time to figure out why, need to get on with the actual task
+                            Console.WriteLine(new CharacterResponseValidation().CheckIfNumber(choice));
+                            if (new CharacterResponseValidation().CheckIfNumber(choice) != true || (choice < '1') || (choice > '2'))
+                            {
+                                Console.WriteLine("Invalid selection. Please try again.\n");
+                            }
+                            switch (choice)
+                            {
+                                case '1':
+                                    box.add(new Box());
+                                    Console.WriteLine("Added a Box");
+                                    break;
+                                case '2':
+                                    box.add(new Product());
+                                    Console.WriteLine("Added a Product");
+                                    break;
+                            }
+                            bool selectionValue = false;
+                            char selectionChar = '0';
                         }
                         break;
                     default:
